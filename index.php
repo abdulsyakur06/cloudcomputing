1683add7c5e27e2c6df1485247240a9ed0ec8805
Abdul Syakur <myhardcore06@gmail.com>
2021-04-29 10:22:51 +0800

    Upload Cloud V 1.0

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	<br> echo " STB : 182228";
-	<br> echo " Nama : Abdul Syakur";
-	<br> echo " KELAS : E";
+	"<br>" echo " STB : 182228";
+	"<br>" echo " Nama : Abdul Syakur";
+	"<br>" echo " KELAS : E";
 	
 ?>
